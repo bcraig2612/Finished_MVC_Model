f2aff48ca41479f1adc5b9ce0ed053a838f77f2c
Brandon <devbrandoncraig@gmail.com>
2020-02-07 11:32:43 -0700

    Fixed Error!

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -15,7 +15,7 @@ class Core {
         $url = $this->getUrl();
         
         // Look in controllers for first value (index)
-        if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')) { 
+        if($url && file_exists('../app/controllers/' . ucwords($url[0]). '.php')) { 
             // If exists, set as controller
             $this->currentController = ucwords($url[0]);
             // Unset 0 Index
